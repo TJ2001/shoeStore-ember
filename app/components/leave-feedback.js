79b1ed507779ba5724b697163fc4128a6b6be3b5
Bo Zhao and Tim Jung <Guest@epicodus-55.local>
2016-08-25 16:09:44 -0700

    add ratings

--- app/components/leave-feedback.js
+++ app/components/leave-feedback.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
         var params = {
           user: this.get('user'),
           comment: this.get('comment'),
+          rating: this.get('rating'),
           shoe: this.get('shoe')
         };
         // this.set('addNewShoe', false);
--- app/models/feedback.js
+++ app/models/feedback.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   user: DS.attr(),
   comment: DS.attr(),
+  rating: DS.attr(),
   shoe: DS.belongsTo('shoe', {async: true})
 });
